a359aff3677518bf2b89b787c9e6cb74eed8c601
StarkOne <Stark.pro40@gmail.com>
2017-05-14 13:42:02 +0700

    js

--- js.js
+++ js.js
@@ -24,13 +24,41 @@ var model = {
 	fire: function(guess){
 		for (var i = 0; i < this.length; i++) {
 			var ship = this.ships[i];
-			locations = ship.locations;
-			var index = locations.indexOf(guess);
+			var index = ship.locations.indexOf(guess);
 			if(index >= 0){
 				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!!");
+				if(this.isSunk(ship)){
+					view.displayMessage("You sank my Battleship");
+					this.shipsSunk++;
+				}
 				return true;
 			}
 		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed");
 		return false;
+	},
+	isSunk: function(ship){
+		for (var i = 0; i < this.shipLength; i++) {
+			if(ship.hits[i] !== "hit"){
+				return false;
+			}
+		}
+		return true;
 	}
 }
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
+model.fire("24");
+model.fire("44"); 
+
+model.fire("12"); 
+model.fire("11"); 
+model.fire("10");
